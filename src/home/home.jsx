b17d3ee305e22e610d867fd234b449c0d1ac6370
Warius <m@rius.me>
2020-04-19 17:24:52 +0200

    Added a favicon to the page.

--- src/home/home.jsx
+++ src/home/home.jsx
@@ -5,14 +5,16 @@ export class home extends Component {
     render() {
         return (
             <div id="home">
-                <h2>Welcome</h2>
-                <p>
-                    Hi, and welcome to rius.me. You can say that this site
-                    breakes the purpose of every site; since it has no such
-                    thing. This is developed for fun purposes only, but if you
-                    against all odds find some features on this site that you
-                    like, be my guest and enjoy them.
-                </p>
+                <div>
+                    <h2>Welcome</h2>
+                    <p>
+                        Hi, and welcome to rius.me. You can say that this site
+                        breakes the purpose of every site; since it has no such
+                        thing. This is developed for fun purposes only, but if
+                        you against all odds find some features on this site
+                        that you like, be my guest and enjoy them.
+                    </p>
+                </div>
             </div>
         );
     }
