c2adaf951c39b4c8901d6aa2e411c1c630760050
Warius <m@rius.me>
2020-04-20 21:19:06 +0200

    added multiple sections to the landing page 🎉

---
+++ build/precache-manifest.9a6195857106bd5422b6d16b7c6d0044.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9717d5f5d770e0235af396517ec9fdd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1f96e56a04aead03873",
+    "url": "/static/css/main.7a2f2c3e.chunk.css"
+  },
+  {
+    "revision": "ffc61076f9f09169aa57",
+    "url": "/static/js/2.dbbf44b3.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.dbbf44b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1f96e56a04aead03873",
+    "url": "/static/js/main.97baa2d1.chunk.js"
+  },
+  {
+    "revision": "fc826a517b5697f66f59",
+    "url": "/static/js/runtime-main.5872e535.js"
+  },
+  {
+    "revision": "1457ad701ef5acfc5c40e44db9aa1cb3",
+    "url": "/static/media/logo2.0.1457ad70.png"
+  },
+  {
+    "revision": "018aa3e7fa72ff9ab0c0c37ade882a33",
+    "url": "/static/media/paaTur.018aa3e7.jpeg"
+  },
+  {
+    "revision": "cbafaa670d315b2398957d51573fe3ad",
+    "url": "/static/media/portrett.cbafaa67.png"
+  },
+  {
+    "revision": "d5f3f9ce51a76263dcbf6b9a5cbe0ac8",
+    "url": "/static/media/sporty.d5f3f9ce.jpeg"
+  }
+]);
--- src/home/home.jsx
+++ src/home/home.jsx
@@ -1,13 +1,36 @@
 import React, { Component } from 'react';
 import './home.css';
+import portrett from '../graphics/portrett.png';
+import sporty from '../graphics/sporty.jpeg';
+import paaTur from '../graphics/paaTur.jpeg';
 
 export class home extends Component {
     render() {
         return (
             <div id="home">
-                <div>
+                <div id="section">
+                    <img src={portrett} alt="Portrett" id="portrett" />
                     <h2>Hei!</h2>
-                    <p>Velkommen til min side.</p>
+                    <p>
+                        Jeg er Marius Sjøberg, en student som studerer
+                        informatikk ved NTNU i Trondeim. Og som du kanskje har
+                        skjønt; en stolt eier av rius.me domenet.
+                    </p>
+                </div>
+                <div id="section">
+                    <img src={sporty} alt="Portrett" id="sporty" />
+                    <p>
+                        Jeg er utrolig glad i å løpe! Så det er ganske enkel
+                        logikk for om jeg løper eller ikke!
+                        <br /> (VondtIKneet) ? LiggerPaaSofaen : erPaaLopetur
+                    </p>
+                </div>
+                <div id="section">
+                    <img src={paaTur} alt="Portrett" id="paaTur" />
+                    <p>
+                        Jeg er veldig glad i å "koble av" med en liten tur! Så
+                        du finner meg fort der dekningen ikke strekker til også.
+                    </p>
                 </div>
             </div>
         );
