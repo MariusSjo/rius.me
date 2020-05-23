127d0a825afce5b1b4b66e1a7bdc8e2cd164ca3d
Warius <m@rius.me>
2020-05-23 23:13:55 +0200

    gjorde vinomonopolsiden til en funksjonell komponent. Har også opprettet en filtreringsmulighet vha en dropdown-meny.🍺

--- src/projects/projects.jsx
+++ src/projects/projects.jsx
@@ -16,24 +16,28 @@ export default class projects extends Component {
                     innhold:
                         'Jeg har lenge vært interessert i redigering og bildemanipulasjon. Det hele startet med enkel manipulering, til faceswapping til nå å lage min egen vektorgrafikk.',
                     bilde: photoshop,
+                    link: '',
                 },
                 {
                     tittel: 'Rius.me',
                     innhold:
                         'For en stund tilbake så bestemte jeg meg for å lage denne siden. Denne har jeg laget på eget initativ på fritiden min. Mye for å få vist hva jeg kan, samtidig som at det er ganske gøy. (Kanskje mest av alt; det at folk klager over at det ikke er noe på rius.me domentet.)',
                     bilde: rius,
+                    link: '',
                 },
                 {
                     tittel: 'Vinduslukker',
                     innhold:
                         'Du vet den følelsen når du legger deg om kvelden på vinteren og det er godt og kalt på rommet. også blir man trø.... Og nå så våkner man i et kaldt rom med vinduet oppe!? Jeg har den perfekte løsningen for deg!.',
                     bilde: vindu,
+                    link: '',
                 },
                 {
-                    tittel: 'Vinmonopolet',
+                    tittel: '',
                     innhold:
-                        'Er du også en av dem som har hatt en hard studietid der man har smakt så å si all øl som Rema1000 har å tilby, men det er litt dyrt å gå for polvarene? Ved hjelp av vinomopolets API er jeg i ferd med å finne løsningen! Så hold ut enn så lenge!',
+                        'Er du også en av dem som har hatt en hard studietid der man har smakt så å si all øl som Rema1000 har å tilby, men det er litt dyrt å gå for polvarene? Ved hjelp av vinomopolets API har jeg funnet løsningen! så hva venter du på? Trykk på tittelen da vel!',
                     bilde: polet,
+                    link: 'Vinmonopolet',
                 },
             ],
         };
@@ -52,16 +56,21 @@ export default class projects extends Component {
                                 ></img>
                             </div>
                             <p>
-                                <h2>{project.tittel}</h2>
+                                <h2>
+                                    <NavLink
+                                        activeClassName="active"
+                                        to="/polet"
+                                    >
+                                        {project.link}
+                                    </NavLink>
+                                    {project.tittel}
+                                </h2>
                                 <hr />
                                 {project.innhold}
                             </p>
                         </div>
                     );
                 })}
-                <NavLink activeClassName="active" to="/polet">
-                    Vinmonopolet
-                </NavLink>
             </div>
         );
     }
--- src/vinmonopolet/vinmonopolet.jsx
+++ src/vinmonopolet/vinmonopolet.jsx
@@ -1,125 +1,127 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import './vinmonopolet.css';
 import searchIcon from '../graphics/search_icon.png';
-export class vinmonopolet extends Component {
-    constructor() {
-        super();
-        this.state = {
-            searchText: '',
-            liste: [],
-        };
-        this.handleChange = this.handleChange.bind(this);
-    }
+function Vinmonopolet() {
+    const [liste, setListe] = useState([]);
+    const [searchText, setSearchText] = useState('');
+    const [alternative, setAlternative] = useState(0);
+    const alternatives = [
+        { name: 'Alle', value: 0, checked: 'checked' },
+        { name: 'Øl', value: 5, checked: '' },
+        { name: 'Vin', value: 1, checked: '' },
+        { name: 'Brennevin', value: 3, checked: '' },
+    ];
 
-    componentDidMount() {
-        try {
-            fetch(
-                'https://apis.vinmonopolet.no/products/v0/details-normal?KEY=6a5497e32c02464c854e54ec27996ab1&maxResults=40'
-            )
-                .then((results) => {
-                    return results.json();
-                })
-                .then((data) => {
-                    this.setState({
-                        liste: data,
-                    });
-                });
-            console.log(this.state.liste);
-            localStorage.setItem('0', this.state.liste);
-        } catch {
-            console.log('not connected');
-        }
-    }
+    useEffect(handleChange, [searchText]);
 
-    async handleChange(e) {
-        await this.setState({
-            searchText: e.target.value.split(' ').join('_'),
-        });
+    function handleChange(liste) {
         try {
             fetch(
                 'https://apis.vinmonopolet.no/products/v0/details-normal?KEY=6a5497e32c02464c854e54ec27996ab1&maxResults=30&productshortnamecontains=' +
-                    this.state.searchText
+                    searchText
             )
                 .then((results) => {
                     return results.json();
                 })
                 .then((data) => {
-                    this.setState({
-                        liste: data,
-                    });
+                    setListe(data);
                 });
-            console.log(this.state.liste);
-            localStorage.setItem('0', this.state.liste);
+            console.log(liste);
         } catch {
             console.log('not connected');
         }
     }
-
-    render() {
-        return (
-            <div>
-                <div id="searchAlco">
-                    <img src={searchIcon} id="searchIcon" alt="SearchIcon" />
-                    <input
-                        id="searchBar"
-                        type="text"
-                        id="searchBar"
-                        placeholder="Søk etter polvarer her!"
-                        value={this.state.value}
-                        onChange={this.handleChange}
-                    />
-                    <p id="APK_info">
-                        *APK: Alkohol per krone. Dette er en måleenhet som
-                        forklarer hvor mye alkohol man får for pengene. For å gi
-                        et perspektiv til forholdstallet så vil en 6-pack med øl
-                        på 4,7% til kr 200 ha 0,0705 APK.
-                    </p>
-                </div>
-                <div id="Poloversikt">
-                    <h3>Resultater:</h3>
-                    {this.state.liste.map((drinks) => {
-                        if (drinks.basic.alcoholContent > 0) {
+    return (
+        <div>
+            <div id="searchAlco">
+                <img src={searchIcon} id="searchIcon" alt="SearchIcon" />
+                <input
+                    id="searchBar"
+                    type="text"
+                    id="searchBar"
+                    placeholder="Søk etter polvarer her!"
+                    onChange={(e) => setSearchText(e.target.value)}
+                />
+                <p id="APK_info">
+                    *APK: Alkohol per krone. Dette er en måleenhet som forklarer
+                    hvor mye alkohol man får for pengene. For å gi et perspektiv
+                    til forholdstallet så vil en 6-pack med øl på 4,7% til kr
+                    200 ha 0,0705 APK.
+                </p>
+                <div id="alternatives">
+                    <select onChange={(e) => setAlternative(e.target.value)}>
+                        {alternatives.map((alt) => {
                             return (
-                                <div
-                                    className="enheter"
-                                    key={drinks.basic.productId}
-                                >
-                                    <div>
-                                        <h4>{drinks.basic.productShortName}</h4>
-                                    </div>
-                                    <img
-                                        className="picture"
-                                        alt={drinks.basic.productLongName}
-                                        src={
-                                            'https://bilder.vinmonopolet.no/cache/200x200-0/' +
-                                            drinks.basic.productId +
-                                            '-1.jpg'
-                                        }
-                                    />
-                                    <div>
-                                        <o>Pris:</o>{' '}
-                                        {drinks.prices[0].salesPrice}kr
-                                        <br />
-                                        Prosent: {
-                                            drinks.basic.alcoholContent
-                                        }% <br />
-                                        Volum: {drinks.basic.volume} <br />
-                                        APK:{' '}
-                                        {Math.round(
-                                            ((drinks.basic.volume *
-                                                drinks.basic.alcoholContent) /
-                                                drinks.prices[0].salesPrice) *
-                                                100
-                                        ) / 100}
-                                    </div>
-                                </div>
+                                <option value={alt.value}>{alt.name}</option>
                             );
-                        }
-                    })}
+                        })}
+                    </select>
                 </div>
             </div>
-        );
-    }
+            <div id="Poloversikt">
+                <h3>Resultater:</h3>
+                {liste.map((drinks) => {
+                    if (drinks.basic.alcoholContent > 0) {
+                        console.log(
+                            drinks.classification.mainProductTypeName.includes(
+                                'vin'
+                            ),
+                            drinks.classification.mainProductTypeId,
+                            alternative,
+                            drinks.classification.mainProductTypeName == 'Øl'
+                        );
+                        if (
+                            drinks.classification.mainProductTypeId ==
+                                alternative ||
+                            alternative == 0
+                        ) {
+                            {
+                                return (
+                                    <div
+                                        className="enheter"
+                                        key={drinks.basic.productId}
+                                    >
+                                        <div>
+                                            <h4>
+                                                {drinks.basic.productShortName}
+                                            </h4>
+                                        </div>
+                                        <img
+                                            className="picture"
+                                            alt={drinks.basic.productLongName}
+                                            src={
+                                                'https://bilder.vinmonopolet.no/cache/200x200-0/' +
+                                                drinks.basic.productId +
+                                                '-1.jpg'
+                                            }
+                                        />
+                                        <div>
+                                            <o>Pris:</o>{' '}
+                                            {drinks.prices[0].salesPrice}kr
+                                            <br />
+                                            Prosent:{' '}
+                                            {drinks.basic.alcoholContent}%{' '}
+                                            <br />
+                                            Volum: {drinks.basic.volume} <br />
+                                            APK:{' '}
+                                            {Math.round(
+                                                ((drinks.basic.volume *
+                                                    drinks.basic
+                                                        .alcoholContent) /
+                                                    drinks.prices[0]
+                                                        .salesPrice) *
+                                                    100
+                                            ) / 100}
+                                        </div>
+                                    </div>
+                                );
+                            }
+                        }
+                    }
+                })}
+            </div>
+        </div>
+    );
 }
 
-export default vinmonopolet;
+export default Vinmonopolet;
