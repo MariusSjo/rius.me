03393a92034a96d08e4fba1c831998b5d427937e
Marius Christopher Sjøberg <mariussjoberg@Mariuss-MacBook-Pro.local>
2020-08-29 13:22:09 +0200

    La til ansiktsgjenkjenning på siden 🤓

--- src/App.js
+++ src/App.js
@@ -5,60 +5,63 @@ import Header from './header/header.jsx';
 import Home from './home/home.jsx';
 import Projects from './projects/projects.jsx';
 import Vinmonopolet from './vinmonopolet/vinmonopolet.jsx';
+import FaceDetection from './faceDetection/faceDetection.jsx';
 import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    NavLink,
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	NavLink,
 } from 'react-router-dom';
 
 function App() {
-    return (
-        <div>
-            <Router>
-                <ul class="navbar">
-                    <div>
-                        <li>
-                            <NavLink activeClassName="active" to="/ ">
-                                Hjem
-                            </NavLink>
-                        </li>
-                        <li id="divider"> | </li>
-                        <li>
-                            <NavLink
-                                activeClassName="active"
-                                to="/projects"
-                                id="projects"
-                            >
-                                Prosjekter
-                            </NavLink>
-                        </li>
-                    </div>
-                    <li>
-                        <NavLink activeClassName="active" to="/contact">
-                            Kontakt
-                        </NavLink>
-                    </li>
-                </ul>
-                <Switch>
-                    <Route path="/projects">
-                        <Projects />
-                    </Route>
-                    <Route path="/contact">
-                        <About />
-                    </Route>
-                    <Route path="/polet">
-                        <Vinmonopolet />
-                    </Route>
-                    <Route path="/">
-                        <Header />
-                        <Home />
-                    </Route>
-                    <Route component={Header} />
-                </Switch>
-            </Router>
-        </div>
-    );
+	return (
+		<div>
+			<Router>
+				<ul class='navbar'>
+					<div>
+						<li>
+							<NavLink activeClassName='active' to='/ '>
+								Hjem
+							</NavLink>
+						</li>
+						<li id='divider'> | </li>
+						<li>
+							<NavLink
+								activeClassName='active'
+								to='/projects'
+								id='projects'>
+								Prosjekter
+							</NavLink>
+						</li>
+					</div>
+					<li>
+						<NavLink activeClassName='active' to='/contact'>
+							Kontakt
+						</NavLink>
+					</li>
+				</ul>
+				<Switch>
+					<Route path='/projects'>
+						<Projects />
+					</Route>
+					<Route path='/contact'>
+						<About />
+					</Route>
+					<Route path='/polet'>
+						<Vinmonopolet />
+					</Route>
+					<Route path='/faceDetection'>
+						<FaceDetection />
+					</Route>
+					<Route path='/'>
+						<Header />
+						<Home />
+					</Route>
+					<Route component={Header} />
+				</Switch>
+			</Router>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/faceDetection/faceDetection.jsx
@@ -0,0 +1,68 @@
+import React, { useEffect, useState } from 'react';
+import './faceDetection.css';
+import * as faceapi from 'face-api.js';
+function FaceDetection() {
+	const MODEL_URL = '/models';
+	useEffect(() => {
+		Promise.all([
+			faceapi.nets.tinyFaceDetector.loadFromUri(MODEL_URL),
+			faceapi.nets.faceLandmark68Net.loadFromUri(MODEL_URL),
+			faceapi.nets.faceRecognitionNet.loadFromUri(MODEL_URL),
+			faceapi.nets.faceExpressionNet.loadFromUri(MODEL_URL),
+		])
+			.then(startVideo())
+			.then(graphics());
+	});
+
+	function startVideo() {
+		const video = document.getElementById('video');
+		navigator.getUserMedia(
+			{
+				video: {},
+			},
+			(stream) => (video.srcObject = stream),
+			(err) => console.log(err)
+		);
+	}
+	function graphics() {
+		const video = document.getElementById('video');
+		video.addEventListener('play', () => {
+			const canvas = faceapi.createCanvasFromMedia(video);
+			document.getElementById('frame').appendChild(canvas);
+			const displaySize = { width: video.width, height: video.height };
+			faceapi.matchDimensions(canvas, displaySize);
+			setInterval(async () => {
+				const detections = await faceapi
+					.detectAllFaces(
+						video,
+						new faceapi.TinyFaceDetectorOptions()
+					)
+					.withFaceLandmarks()
+					.withFaceExpressions();
+				const rezizedDetections = faceapi.resizeResults(
+					detections,
+					displaySize
+				);
+				canvas
+					.getContext('2d')
+					.clearRect(0, 0, canvas.width, canvas.height);
+				faceapi.draw.drawFaceLandmarks(canvas, rezizedDetections);
+				faceapi.draw.drawDetections(canvas, rezizedDetections);
+			}, 100);
+		});
+	}
+	return (
+		<div>
+			<div id='frame'>
+				<video
+					id='video'
+					width='720'
+					height='560'
+					autoPlay
+					muted></video>
+			</div>
+		</div>
+	);
+}
+
+export default FaceDetection;
