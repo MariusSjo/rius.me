0ef86dac28564950e22e5c7005403e0344025c6f
Marius Christopher Sjøberg <mariussjoberg@Mariuss-MacBook-Pro.local>
2020-11-10 22:28:23 +0100

    Made the landingpage even more responsive! 💅

--- src/home/home.jsx
+++ src/home/home.jsx
@@ -10,7 +10,7 @@ export class home extends Component {
 		return (
 			<div id='home'>
 				<div className='col-md-7'>
-					<div>
+					<div className='text-home'>
 						<h2>Fyttigrisen så heldig jeg er... </h2>
 						<p>
 							som fikk deg på besøk! Jeg er Marius Sjøberg, en
@@ -23,12 +23,12 @@ export class home extends Component {
 				</div>
 
 				<div className='col-md-7 invertedColumn'>
-					<div>
+					<div className='text-home'>
 						<h2>Dette er nok langtifra din nye hjemmeside!</h2>
 						<p>
 							Men du skal ikke se bortifra at du finner
 							funksjonalitet som du vil få bruk for en dag.
-							(Hvertfall hvis du vil spare litt penger.{' '}
+							(Hvertfall hvis du vil spare litt penger:{' '}
 							<NavLink to='/vinmonopolet'> vinmonopolet</NavLink>)
 						</p>
 					</div>
@@ -36,9 +36,9 @@ export class home extends Component {
 				</div>
 
 				<div class='col-md-7'>
-					<div>
+					<div className='text-home'>
 						<h2>Sjekk gjerne ut prosjektene mine! </h2>
-						<p className='lead'>
+						<p>
 							Siden du har tatt deg tid til å lese så langt, så
 							anbefaler jeg deg hvertfall å ta en titt på
 							prosjektene mine! Tar gjerne imot litt input også.
