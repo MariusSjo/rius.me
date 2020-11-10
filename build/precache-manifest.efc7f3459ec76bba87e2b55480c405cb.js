51bf518bdfda806e28d9bd721f6a9cb357f14a4a
Marius Christopher Sjøberg <mariussjoberg@Mariuss-MacBook-Pro.local>
2020-11-10 22:01:15 +0100

    changed the description on the landingpage

---
+++ build/precache-manifest.efc7f3459ec76bba87e2b55480c405cb.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01de3e32a6db7d6f198528bbd4bccb4a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "23833d84f1e466aa0f96",
+    "url": "./static/css/main.f9170ad5.chunk.css"
+  },
+  {
+    "revision": "86f3c38a7cc833c2f3cd",
+    "url": "./static/js/2.22b538bb.chunk.js"
+  },
+  {
+    "revision": "1030b4c22ec2e703698a87d7ca796958",
+    "url": "./static/js/2.22b538bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23833d84f1e466aa0f96",
+    "url": "./static/js/main.28b501ba.chunk.js"
+  },
+  {
+    "revision": "8318ca6f3f080a05b432",
+    "url": "./static/js/runtime-main.30417107.js"
+  },
+  {
+    "revision": "c8b6e083af3f94009801989c3739425e",
+    "url": "./static/media/Montserrat-Medium.c8b6e083.ttf"
+  },
+  {
+    "revision": "4b84cbc550693ed938292856b082e4b9",
+    "url": "./static/media/face.4b84cbc5.png"
+  },
+  {
+    "revision": "d6e4341253048c52680a7e4aefb1295b",
+    "url": "./static/media/ferie.d6e43412.png"
+  },
+  {
+    "revision": "f8bae06f23b492ac43070809a57048ad",
+    "url": "./static/media/nyfont.f8bae06f.png"
+  },
+  {
+    "revision": "bb77f7878b3ea77479531adf077654dc",
+    "url": "./static/media/paaTur.bb77f787.jpg"
+  },
+  {
+    "revision": "e7b058fd414fbacfc29a29edbaadb5ab",
+    "url": "./static/media/photoshop.e7b058fd.png"
+  },
+  {
+    "revision": "bf6897410f063a212f9ed568d7483f60",
+    "url": "./static/media/polet.bf689741.png"
+  },
+  {
+    "revision": "605958c4f66e584ade2090e6c8fc6eaa",
+    "url": "./static/media/skitur.605958c4.jpg"
+  },
+  {
+    "revision": "638456d5fc56be92d03a6b2e443b03c6",
+    "url": "./static/media/vindu.638456d5.png"
+  },
+  {
+    "revision": "74c1e8cede1af3e8344a9c1f208455b8",
+    "url": "./static/media/visittkort.74c1e8ce.jpg"
+  }
+]);
--- src/home/home.jsx
+++ src/home/home.jsx
@@ -3,16 +3,15 @@ import './home.css';
 import ferie from '../graphics/ferie.png';
 import paaTur from '../graphics/paaTur.jpg';
 import portrett from '../graphics/skitur.jpg';
+import { BrowserRouter as Router, NavLink } from 'react-router-dom';
 
 export class home extends Component {
 	render() {
 		return (
 			<div id='home'>
-				<div class='col-md-7'>
+				<div className='col-md-7'>
 					<div>
-						<h2 class='featurette-heading'>
-							Fyttigrisen så heldig jeg er...{' '}
-						</h2>
+						<h2>Fyttigrisen så heldig jeg er... </h2>
 						<p>
 							som fikk deg på besøk! Jeg er Marius Sjøberg, en
 							student som studerer informatikk ved NTNU i
@@ -25,14 +24,12 @@ export class home extends Component {
 
 				<div className='col-md-7 invertedColumn'>
 					<div>
-						<h2 className='featurette-heading'>
-							Så hyggelig at du tok turen!{' '}
-						</h2>
+						<h2>Dette er nok langtifra din nye hjemmeside!</h2>
 						<p>
-							Jeg er veldig glad i å "koble av" med en liten tur!
-							Så du finner meg fort der dekningen ikke strekker
-							til også. Om du har en stravabruker må du ikke kvie
-							deg for å ta kontakt.
+							Men du skal ikke se bortifra at du finner
+							funksjonalitet som du vil få bruk for en dag.
+							(Hvertfall hvis du vil spare litt penger.{' '}
+							<NavLink to='/vinmonopolet'> vinmonopolet</NavLink>)
 						</p>
 					</div>
 					<img src={portrett} alt='Portrett' />
@@ -40,17 +37,15 @@ export class home extends Component {
 
 				<div class='col-md-7'>
 					<div>
-						<h2 className='featurette-heading'>
-							Sjekk gjerne ut prosjektene mine!{' '}
-						</h2>
-						<p class='lead'>
+						<h2>Sjekk gjerne ut prosjektene mine! </h2>
+						<p className='lead'>
 							Siden du har tatt deg tid til å lese så langt, så
 							anbefaler jeg deg hvertfall å ta en titt på
 							prosjektene mine! Tar gjerne imot litt input også.
 							Ha en finfin dag videre.
 						</p>
 					</div>
-					<div class=''>
+					<div>
 						<img src={paaTur} alt='Portrett' id='portrett' />
 					</div>
 				</div>
